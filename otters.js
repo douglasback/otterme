2613de671f923693a2d38d8372e1bb240313495e
Douglas Back <douglas@bluestatedigital.com>
2012-01-26 11:25:15 -0500

    Added MOAR OTTERS

--- otters.js
+++ otters.js
@@ -6,7 +6,10 @@ var otters = { "photos" : [
     "http://dl.dropbox.com/u/14236910/otters/PamYM.jpg",
     "http://dl.dropbox.com/u/14236910/otters/September-01-2011-22-25-00-photo.jpeg",
     "http://dl.dropbox.com/u/14236910/otters/tumblr_lxoxhwkWIq1qzs75go1_500.jpg",
-    "http://dl.dropbox.com/u/14236910/otters/utI2U.jpg"
+    "http://dl.dropbox.com/u/14236910/otters/utI2U.jpg",
+    "http://utterlycute.com/wp-content/uploads/2011/09/Otters.jpg",
+    "http://chelseasthoughts.files.wordpress.com/2010/12/otters.jpg",
+    "http://www.kindredspiritsproject.com/wp-content/uploads/2011/05/Sea_otters_holding_hands.jpg"
 ]};
 
 exports.otters = otters;

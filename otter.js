01d12b9061de3dccf7645544ae0e32028157b71c
Douglas Back <douglas@bluestatedigital.com>
2012-01-26 10:09:04 -0500

    initial commit

---
+++ otter.js
@@ -0,0 +1,23 @@
+var app = require("express").createServer();
+var otters = require("otters").otters;
+
+otters.random = function(){
+    return { "otter" : otters.photos[Math.floor(Math.random() * otters.photos.length);] }
+}
+
+app.get('/', function(req, res){
+    res.contentType('text/plain');
+    res.send("Here be Otters");
+});
+app.get('/random', function(req, res){
+    res.contentType('application/json');
+    res.send(otters.random());
+});
+
+
+app.get('/count', function(req, res){
+    res.contentType('text/plain');
+    res.send("Here be " + otters.photos.length + " otters.");
+});
+
+
---
+++ otters.js
@@ -0,0 +1,12 @@
+var otters = { "photos" : [
+    "http://dl.dropbox.com/u/14236910/otters/848px-LutraCanadensis_fullres.jpg",
+    "http://dl.dropbox.com/u/14236910/otters/bTol3.jpg",
+    "http://dl.dropbox.com/u/14236910/otters/enhanced-buzz-440-1305902425-8.jpg",
+    "http://dl.dropbox.com/u/14236910/otters/Milo-and-Tanu-Otters-rafting-holding-hands-John-Healey-110803_DSC6427-750x440.jpg",
+    "http://dl.dropbox.com/u/14236910/otters/PamYM.jpg",
+    "http://dl.dropbox.com/u/14236910/otters/September-01-2011-22-25-00-photo.jpeg",
+    "http://dl.dropbox.com/u/14236910/otters/tumblr_lxoxhwkWIq1qzs75go1_500.jpg",
+    "http://dl.dropbox.com/u/14236910/otters/utI2U.jpg"
+]};
+
+exports.otters = otters;

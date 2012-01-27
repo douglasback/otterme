91e4b6a9d1968eb49d0b6266e1a2bf08e60601a1
Douglas Back <douglasback@mac.com>
2012-01-26 23:31:57 -0500

    EVEN MOAR OTTERS

--- otters.js
+++ otters.js
@@ -9,7 +9,12 @@ var otters = { "photos" : [
     "http://dl.dropbox.com/u/14236910/otters/utI2U.jpg",
     "http://utterlycute.com/wp-content/uploads/2011/09/Otters.jpg",
     "http://chelseasthoughts.files.wordpress.com/2010/12/otters.jpg",
-    "http://www.kindredspiritsproject.com/wp-content/uploads/2011/05/Sea_otters_holding_hands.jpg"
+    "http://www.kindredspiritsproject.com/wp-content/uploads/2011/05/Sea_otters_holding_hands.jpg",
+    "http://www.factzoo.com/sites/all/img/mammals/north_american_river_otter_face.jpg",
+    "http://www.otterproject.org/atf/cf/%7B1032ABCB-19F9-4CB6-8364-2F74F73B3013%7D/Otter_Image1.jpg",
+    "http://www.eaglewingtours.com/userimages/Image/sea-otter.jpg",
+    "http://www.otterproject.org/atf/cf/%7B1032ABCB-19F9-4CB6-8364-2F74F73B3013%7D/OTTER02.JPG",
+    "http://www.splashswimschool.com/Portals/81129/images/RIver%20Otter-resized-600.jpg"
 ]};
 
 exports.otters = otters;

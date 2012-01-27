f2b2721530d0e0b05d80bd3090245a370f5c9a24
Douglas Back <douglasback@mac.com>
2012-01-26 23:24:41 -0500

    Added bomb method and route

--- otterme.js
+++ otterme.js
@@ -5,9 +5,16 @@ otters.random = function(){
     return { "otter" : otters.photos[Math.floor(Math.random() * otters.photos.length)] };
 };
 
-otters.bomb = function(){
+otters.bomb = function(num){
+    var payload = { "otter_payload" : [] },
+        joy = num < otters.photos.length ? num : otters.photos.length,
+        tmpOtters = otters.photos.slice(0,otters.photos.length);
     
-}
+        for (var i=0; i < joy; i++){
+            payload.otter_payload.push(tmpOtters.splice([Math.floor(Math.random() * tmpOtters.length)], 1)[0]);
+        }
+    return payload.otter_payload.length > 0 ? payload : null;
+};
 
 app.get('/', function(req, res){
     res.contentType('text/html');
@@ -30,7 +37,9 @@ app.get('/count', function(req, res){
     res.send({ "otter_count" : otters.photos.length + 1 });
 });
 app.get('/bomb/:number', function(req,res){
-    
+    res.contentType('application/json');
+    res.send(otters.bomb(req.param("number")));
+    console.log("Length of otters: " + otters.photos.length);
 });
 
 var port = process.env.PORT || 3000;
